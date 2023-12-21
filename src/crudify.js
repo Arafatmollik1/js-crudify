0c27bb3fd1432fdd39c244f32ca58f25081de195
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2023-12-21 16:07:54 +0200

    fix helpers, and create a test case

--- src/crudify.js
+++ src/crudify.js
@@ -2,16 +2,34 @@ const config = require('./config');
 const Helpers = require('./helpers');
 
 class Crudify {
+  static activeConnectionName = null; // Property to store active connection name
+
   static config(dbConfig) {
     return config.setup(dbConfig);
   }
 
   static ready(connectionName) {
+    Crudify.activeConnectionName = connectionName;
+  }
+  static die(connectionName) {
     const connection = config.getConnection(connectionName);
     if (!connection) {
       throw new Error(`No connection found for name: ${connectionName}`);
     }
-    return connection;
+
+    connection.end(err => {
+      if (err) {
+        throw new Error('Error closing the database connection: ' + err.message);
+      }
+      console.log(`Connection ${connectionName} closed successfully.`);
+    });
+  }
+
+  static getConnectionName() {
+    if (!Crudify.activeConnectionName) {
+      throw new Error('No active database connection. Call Crudify.ready(connectionName) first.');
+    }
+    return Crudify.activeConnectionName;
   }
 
   static create(data) {
@@ -21,8 +39,9 @@ class Crudify {
         const keys = Object.keys(data).join(', ');
         const values = Helpers.escapeValues(data).join(', ');
         const query = `INSERT INTO ${tableName} (${keys}) VALUES (${values})`;
-
-        const connection = config.getConnection(/* connectionName */);
+        console.log(query);
+        const connectionName = Crudify.getConnectionName();
+        const connection = config.getConnection(connectionName);
         return new Promise((resolve, reject) => {
           connection.query(query, (error, results) => {
             if (error) reject(error);
@@ -40,8 +59,9 @@ class Crudify {
           from: async (tableName) => {
             Helpers.validateIdentifier(tableName);
             const query = `SELECT ${columns} FROM ${tableName} WHERE ${condition}`;
-            
-            const connection = config.getConnection(/* connectionName */);
+            console.log(query);
+            const connectionName = Crudify.getConnectionName();
+            const connection = config.getConnection(connectionName);
             return new Promise((resolve, reject) => {
               connection.query(query, (error, results) => {
                 if (error) reject(error);
@@ -62,8 +82,9 @@ class Crudify {
             Helpers.validateIdentifier(tableName);
             const updates = Helpers.buildSetClause(data);
             const query = `UPDATE ${tableName} SET ${updates} WHERE ${condition}`;
-
-            const connection = config.getConnection(/* connectionName */);
+            console.log(query);
+            const connectionName = Crudify.getConnectionName();
+            const connection = config.getConnection(connectionName);
             return new Promise((resolve, reject) => {
               connection.query(query, (error, results) => {
                 if (error) reject(error);
@@ -83,8 +104,9 @@ class Crudify {
           from: async (tableName) => {
             Helpers.validateIdentifier(tableName);
             const query = `DELETE FROM ${tableName} WHERE ${condition}`;
-
-            const connection = config.getConnection(/* connectionName */);
+            console.log(query);
+            const connectionName = Crudify.getConnectionName();
+            const connection = config.getConnection(connectionName);
             return new Promise((resolve, reject) => {
               connection.query(query, (error, results) => {
                 if (error) reject(error);
---
+++ src/test.js
@@ -0,0 +1,99 @@
+const Crudify = require('./index'); // Import your Crudify package
+
+// Database configuration
+const dbConfig = {
+  host: DB_HOST, // replace with your DB host
+  user: DB_USER, // replace with your DB username
+  password: DB_PASSWORD, // replace with your DB password
+  database: DB_DATABASE,  // replace with your DB name
+};
+
+// Configure Crudify to use your database
+Crudify.config({
+    name: 'myTestDB',
+    ...dbConfig
+  });
+  
+
+// Make the connection ready
+Crudify.ready('myTestDB');
+
+// Create
+async function testCreate() {
+    try {
+      const data = {
+        email: 'fromcrudify@crudify.com',
+        usertype: 'a'
+      };
+  
+      const result = await Crudify.create(data).into('webuser');
+      console.log('Create Result:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  // Read
+  async function testRead() {
+    try {
+      const result = await Crudify.read('*').where('1').from('webuser');
+      console.log('Read Result:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  // Update
+  async function testUpdate() {
+    try {
+      const newData = {
+        usertype: 'b'
+      };
+  
+      const result = await Crudify.update(newData).from('webuser').where("email = 'fromcrudify@crudify.com'");
+      console.log('Update Result:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  // Delete
+  async function testDelete() {
+    try {
+      const result = await Crudify.delete().where("email = 'fromcrudify@crudify.com'").from('webuser');
+      console.log('Delete Result:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+    // Die (Close connection)
+    async function testDie() {
+        try {
+            Crudify.die('myTestDB');
+            console.log('Connection closed successfully.');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+    function logGreen(text) {
+        console.log(`\x1b[32m${text}\x1b[0m`); // Green
+      }
+
+  async function runTests() {
+    logGreen("---------The following is the result for creating an entry------------");
+    await testCreate();
+    logGreen("---------The following is the result for reading an entry------------");
+    await testRead();
+    logGreen("---------The following is the result for updating an entry------------");
+    await testUpdate();
+    logGreen("---------The following is the result for reading an entry once again after update------------");
+    await testRead(); 
+    logGreen("---------The following is the result for deleting an entry------------");
+    await testDelete();
+    logGreen("---------The following is the result for reading an entry after deletion------------");
+    await testRead();
+    logGreen("---------The following is the result for closing the database connection------------");
+    await testDie();
+  }
+  
+  runTests();
